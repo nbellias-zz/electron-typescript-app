d55a5201ac578b28cb14df2aff5d042be3aca25e
Nikolaos Bellias <nikolaos.bellias@hotmail.com>
2022-05-07 08:55:40 +0300

    Initial Commit

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+class Main {
+    static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+    }
+    static onClose() {
+        // Dereference the window object. 
+        Main.mainWindow.close;
+    }
+    static onReady() {
+        Main.mainWindow = new Main.BrowserWindow({ width: 800, height: 600 });
+        Main.mainWindow
+            .loadURL('file://' + __dirname + '/index.html');
+        Main.mainWindow.on('closed', Main.onClose);
+    }
+    static main(app, browserWindow) {
+        // we pass the Electron.App object and the  
+        // Electron.BrowserWindow into this function 
+        // so this class has no dependencies. This 
+        // makes the code easier to write tests for 
+        Main.BrowserWindow = browserWindow;
+        electron_1.Menu.setApplicationMenu(null);
+        Main.application = app;
+        Main.application.on('window-all-closed', Main.onWindowAllClosed);
+        Main.application.on('ready', Main.onReady);
+    }
+}
+exports.default = Main;
---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { BrowserWindow, Menu } from 'electron';
+
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+    static BrowserWindow: any;
+    static Menu: Electron.Menu;
+    
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+    }
+
+    private static onClose() {
+        // Dereference the window object. 
+        Main.mainWindow.close;
+    }
+
+    private static onReady() {
+        Main.mainWindow = new Main.BrowserWindow({ width: 800, height: 600 });
+        Main.mainWindow
+            .loadURL('file://' + __dirname + '/index.html');
+        Main.mainWindow.on('closed', Main.onClose);
+    }
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow) {
+        // we pass the Electron.App object and the  
+        // Electron.BrowserWindow into this function 
+        // so this class has no dependencies. This 
+        // makes the code easier to write tests for 
+        Main.BrowserWindow = browserWindow;
+        Menu.setApplicationMenu(null);
+        Main.application = app;
+        Main.application.on('window-all-closed', Main.onWindowAllClosed);
+        Main.application.on('ready', Main.onReady);
+    }
+}
